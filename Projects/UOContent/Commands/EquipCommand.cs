cd6ae55e1350f42a6a3e5fa164ed4dfd3a407a33
Andrew Barber <drewbarber72@gmail.com>
2025-04-02 18:50:09 -0400

    Added maintenance of EquipSet tags on accounts, to remove tags for characters that do not exist and other cases.

--- Projects/UOContent/Commands/EquipCommand.cs
+++ Projects/UOContent/Commands/EquipCommand.cs
@@ -1,5 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Server.Accounting;
 using Server.Mobiles;
 
@@ -36,6 +40,7 @@ public class WeaponPair
         OneHanded = oneHanded;
         TwoHanded = twoHanded;
     }
+
     public WeaponPair(string serializedString)
     {
         if (string.IsNullOrWhiteSpace(serializedString))
@@ -57,18 +62,26 @@ public class WeaponPair
             catch { TwoHanded = null; }
         }
     }
+
+    public Item? OneHanded { get; set; } = null;
+
+    public Item? TwoHanded { get; set; } = null;
+
     public string Serialize()
     {
         return (OneHanded?.Serial.Value ?? 0) + "|" + (TwoHanded?.Serial.Value ?? 0);
     }
-    public Item? OneHanded { get; set; } = null;
-    public Item? TwoHanded { get; set; } = null;
 }
 
 public static class EquipCommand
 {
+    private static readonly string LogFilePath = Path.Combine("Logs", "EquipSetWipeMaintLog.txt");
+
     private static Dictionary<uint, CharacterWeaponSets> m_playerWeaponsCache
-        = new Dictionary<uint, CharacterWeaponSets>();
+            = new Dictionary<uint, CharacterWeaponSets>();
+
+    private static bool maintHasRun = false;
+    private static object maintLock = new object();
 
     public static void Configure()
     {
@@ -77,23 +90,11 @@ public static class EquipCommand
         CommandSystem.Register("UnEquip", AccessLevel.Player, UnEquip_OnCommand);
     }
 
-    private static void InitializeEquipItemAccountCache(PlayerMobile pm)
+    private static void DeleteInvalidEquipSetTag(AccountTag tag, Account account, string reason)
     {
-        if (m_playerWeaponsCache.ContainsKey(pm.Serial.Value)) return;
-        var account = pm.Account as Account;
-        if (account == null) return;
-        m_playerWeaponsCache.Add(pm.Serial.Value, new CharacterWeaponSets());
-        for (int x = 0; x <= 9; x++)
-        {
-            string val = account.GetTag(GetEquipItemSetKeyString(pm, x));
-            if (string.IsNullOrWhiteSpace(val)) continue;
-            m_playerWeaponsCache[pm.Serial.Value][x] = new WeaponPair(val);
-        }
-    }
-
-    private static string GetEquipItemSetKeyString(PlayerMobile pm, int setId)
-    {
-        return $"EquipSet_{pm.Serial.Value}_{setId}";
+        account.RemoveTag(tag.Name);
+        string line = $"Deleted invalid EquipSet tag: {tag.Name}:{tag.Value} from account {account.Username} for reason: {reason}";
+        WriteLogEntry(line);
     }
 
     [Usage("Equip (set#)")]
@@ -152,6 +153,88 @@ public static class EquipCommand
             pm.PlaySound(0x48);
     }
 
+    private static string GetEquipItemSetKeyString(PlayerMobile pm, int setId)
+    {
+        return $"EquipSet_{pm.Serial.Value}_{setId}";
+    }
+
+    private static void InitializeEquipItemAccountCache(PlayerMobile pm)
+    {
+        RunMaint();
+        if (m_playerWeaponsCache.ContainsKey(pm.Serial.Value)) return;
+        var account = pm.Account as Account;
+        if (account == null) return;
+        m_playerWeaponsCache.Add(pm.Serial.Value, new CharacterWeaponSets());
+        for (int x = 0; x <= 9; x++)
+        {
+            string val = account.GetTag(GetEquipItemSetKeyString(pm, x));
+            if (string.IsNullOrWhiteSpace(val)) continue;
+            m_playerWeaponsCache[pm.Serial.Value][x] = new WeaponPair(val);
+        }
+    }
+
+    private static void RunMaint()
+    {
+        lock (maintLock)
+        {
+            if (maintHasRun) return;
+            maintHasRun = true;
+        }
+        Task.Run(async () =>
+        {
+            await Task.Delay(1000);
+            var allAccounts = Accounts.GetAccounts();
+            foreach (var act in allAccounts)
+            {
+                try
+                {
+                    //Tag related properties and methods only exist on the Account class,
+                    //not on the IAccount interface, so we need to cast to Account.
+                    var account = act as Account;
+                    var allTags = account.Tags.ToList();
+                    foreach (var tag in allTags)
+                    {
+                        //only check tags that are EquipSet_ tags
+                        if (tag.Name.StartsWith("EquipSet_"))
+                        {
+                            var parts = tag.Name.Split('_');
+                            //if the tag doesn't have 3 parts, it's invalid
+                            if (parts.Length != 3)
+                            {
+                                DeleteInvalidEquipSetTag(tag, account, "Tag name appears invalid.");
+                                continue;
+                            }
+                            uint serial = 0;
+                            //if the serial isn't a valid uint, it's invalid
+                            if (!uint.TryParse(parts[1], out serial))
+                            {
+                                DeleteInvalidEquipSetTag(tag, account, "character serial is not a valid uint");
+                                continue;
+                            }
+                            var pm = World.FindMobile((Serial)serial) as PlayerMobile;
+                            //if the player isn't found or is deleted, it's invalid
+                            if (pm == null || pm.Deleted)
+                            {
+                                DeleteInvalidEquipSetTag(tag, account, "Character does not exist");
+                                continue;
+                            }
+                            int setId = 0;
+                            //if the setId isn't a valid int between 0 and 9, it's invalid
+                            if (!int.TryParse(parts[2], out setId) || setId < 0 || setId > 9)
+                            {
+                                DeleteInvalidEquipSetTag(tag, account, "SetId out of 0-9 range");
+                                continue;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        });
+    }
+
     [Usage("SetEquip (set#)")]
     [Description("Used to save the current item(s) in hands to a set.")]
     private static void SetEquip_OnCommand(CommandEventArgs e)
@@ -188,6 +271,7 @@ public static class EquipCommand
             return;
         UnequipHands(pm, null);
         pm.PlaySound(0x57);
+        InitializeEquipItemAccountCache(pm);
     }
 
     private static void UnequipHands(PlayerMobile pm, WeaponPair exemptions)
@@ -201,6 +285,15 @@ public static class EquipCommand
         if (twoHanded != null && twoHanded != exemptions?.TwoHanded)
         {
             pm.Backpack.DropItem(twoHanded);
+        }
+    }
+
+    [Conditional("DEBUG")]
+    private static void WriteLogEntry(string line)
+    {
+        using (var sw = new StreamWriter(LogFilePath, true))
+        {
+            sw.WriteLine($"{DateTime.Now.ToString("s")} :{line}");
         }
     }
 }
