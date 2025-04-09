c562bee653735c89ccc921823ccf7714b7b6dfd2
Andrew Barber <drewbarber72@gmail.com>
2025-04-09 17:55:57 -0400

    RuccisCommandMaint.cs now handles maintenance for both Equip and Sort command account tags.

--- Projects/UOContent/Commands/Maint/RuccisCommandMaint.cs
+++ Projects/UOContent/Commands/Maint/RuccisCommandMaint.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Server.Accounting;
@@ -36,38 +37,8 @@ namespace Server.Commands.Maint
                         var allTags = account.Tags.ToList();
                         foreach (var tag in allTags)
                         {
-                            //only check tags that are EquipSet_ tags
-                            if (tag.Name.StartsWith("EquipSet_"))
-                            {
-                                var parts = tag.Name.Split('_');
-                                //if the tag doesn't have 3 parts, it's invalid
-                                if (parts.Length != 3)
-                                {
-                                    DeleteInvalidEquipSetTag(tag, account, "Tag name appears invalid.");
-                                    continue;
-                                }
-                                uint serial = 0;
-                                //if the serial isn't a valid uint, it's invalid
-                                if (!uint.TryParse(parts[1], out serial))
-                                {
-                                    DeleteInvalidEquipSetTag(tag, account, "character serial is not a valid uint");
-                                    continue;
-                                }
-                                var pm = World.FindMobile((Serial)serial) as PlayerMobile;
-                                //if the player isn't found or is deleted, it's invalid
-                                if (pm == null || pm.Deleted)
-                                {
-                                    DeleteInvalidEquipSetTag(tag, account, "Character does not exist");
-                                    continue;
-                                }
-                                int setId = 0;
-                                //if the setId isn't a valid int between 0 and 9, it's invalid
-                                if (!int.TryParse(parts[2], out setId) || setId < 0 || setId > 9)
-                                {
-                                    DeleteInvalidEquipSetTag(tag, account, "SetId out of 0-9 range");
-                                    continue;
-                                }
-                            }
+                            CheckEquipTags(account, tag);
+                            CheckSortTags(account, tag);
                         }
                     }
                     catch (Exception ex)
@@ -76,10 +47,75 @@ namespace Server.Commands.Maint
                 }
             });
         }
-        private static void DeleteInvalidEquipSetTag(AccountTag tag, Account account, string reason)
+
+        private static void CheckSortTags(Account account, AccountTag tag)
+        {
+            //only check tags that are EquipSet_ tags
+            if (tag.Name.StartsWith("SortBagSet_"))
+            {
+                var parts = tag.Name.Split('_');
+                //if the tag doesn't have 2 parts, it's invalid
+                if (parts.Length != 2)
+                {
+                    DeleteInvalidAccountTag(tag, account, "Tag name appears invalid.");
+                    return;
+                }
+                uint serial = 0;
+                //if the serial isn't a valid uint, it's invalid
+                if (!uint.TryParse(parts[1], out serial))
+                {
+                    DeleteInvalidAccountTag(tag, account, "character serial is not a valid uint");
+                    return;
+                }
+                var pm = World.FindMobile((Serial)serial) as PlayerMobile;
+                //if the player isn't found or is deleted, it's invalid
+                if (pm == null || pm.Deleted)
+                {
+                    DeleteInvalidAccountTag(tag, account, "Character does not exist");
+                    return;
+                }
+            }
+        }
+        private static void CheckEquipTags(Account account, AccountTag tag)
+        {
+            //only check tags that are EquipSet_ tags
+            if (tag.Name.StartsWith("EquipSet_"))
+            {
+                var parts = tag.Name.Split('_');
+                //if the tag doesn't have 3 parts, it's invalid
+                if (parts.Length != 3)
+                {
+                    DeleteInvalidAccountTag(tag, account, "Tag name appears invalid.");
+                    return;
+                }
+                uint serial = 0;
+                //if the serial isn't a valid uint, it's invalid
+                if (!uint.TryParse(parts[1], out serial))
+                {
+                    DeleteInvalidAccountTag(tag, account, "character serial is not a valid uint");
+                    return;
+                }
+                var pm = World.FindMobile((Serial)serial) as PlayerMobile;
+                //if the player isn't found or is deleted, it's invalid
+                if (pm == null || pm.Deleted)
+                {
+                    DeleteInvalidAccountTag(tag, account, "Character does not exist");
+                    return;
+                }
+                int setId = 0;
+                //if the setId isn't a valid int between 0 and 9, it's invalid
+                if (!int.TryParse(parts[2], out setId) || setId < 0 || setId > 9)
+                {
+                    DeleteInvalidAccountTag(tag, account, "SetId out of 0-9 range");
+                    return;
+                }
+            }
+        }
+
+        private static void DeleteInvalidAccountTag(AccountTag tag, Account account, string reason, [CallerMemberName] string callerName = "")
         {
             account.RemoveTag(tag.Name);
-            string line = $"Deleted invalid EquipSet tag: {tag.Name}:{tag.Value} from account {account.Username} for reason: {reason}";
+            string line = $"{callerName}: Deleted invalid tag: {tag.Name}:{tag.Value} from account {account.Username} for reason: {reason}";
             WriteLogEntry(line);
         }
         private static void WriteLogEntry(string line)
