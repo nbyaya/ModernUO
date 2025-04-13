eed135ec231ebb8ddfa5b61c54742772591b939a
Andrew Barber <drewbarber72@gmail.com>
2025-04-13 12:39:29 -0400

    Added MessageHues static class to hold constants for message hues.

---
+++ Projects/UOContent/Misc/MessageHues.cs
@@ -0,0 +1,11 @@
+
+namespace Server;
+
+public static class MessageHues
+{
+    public const int RedErrorHue = 0x22;
+    public const int YellowNoticeHue = 0x58;
+    public const int GreenSuccessHue = 0x3B2;
+    public const int LightBlueSystemHue = 0x35;
+    public const int DefaultWhiteHue = 0x5;
+}
