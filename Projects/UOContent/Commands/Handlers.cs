57deb526612c327386cecc2125f9a294f83bf5a6
Andrew Barber <drewbarber72@gmail.com>
2025-04-27 02:58:03 -0400

    [help now opens the Rucci's Wiki Readme page on the repository site.

--- Projects/UOContent/Commands/Handlers.cs
+++ Projects/UOContent/Commands/Handlers.cs
@@ -578,51 +578,11 @@ namespace Server.Commands
         }
 
         [Usage("Help")]
-        [Description("Lists all available commands.")]
+        [Description("Opens Rucci's Player Readme Page")]
         public static void Help_OnCommand(CommandEventArgs e)
         {
-            var m = e.Mobile;
-
-            var list = new List<CommandEntry>();
-
-            foreach (var entry in CommandSystem.Entries.Values)
-            {
-                if (m.AccessLevel >= entry.AccessLevel)
-                {
-                    list.Add(entry);
-                }
-            }
-
-            list.Sort();
-
-            var sb = new StringBuilder();
-
-            if (list.Count > 0)
-            {
-                sb.Append(list[0].Command);
-            }
-
-            for (var i = 1; i < list.Count; ++i)
-            {
-                var v = list[i].Command;
-
-                if (sb.Length + 1 + v.Length >= 256)
-                {
-                    m.SendAsciiMessage(0x482, sb.ToString());
-                    sb = new StringBuilder();
-                    sb.Append(v);
-                }
-                else
-                {
-                    sb.Append(' ');
-                    sb.Append(v);
-                }
-            }
-
-            if (sb.Length > 0)
-            {
-                m.SendAsciiMessage(0x482, sb.ToString());
-            }
+            var pm = e.Mobile as PlayerMobile;
+            pm.LaunchBrowser("https://github.com/colcomet/ModernUO/wiki/Rucci's-UO-Shard-Player-Readme");
         }
 
         [Usage("SMsg <text>")]
