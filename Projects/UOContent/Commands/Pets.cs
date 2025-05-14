9d489881740737bc72218971927453cad68469a6
Andrew Barber <drewbarber72@gmail.com>
2025-05-14 00:59:09 -0400

    Added basic [pets command, to list pets, their locations, and control slots.

---
+++ Projects/UOContent/Commands/Pets.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Server.Gumps;
+using Server.Mobiles;
+
+namespace Server.Commands
+{
+    public static class Pets
+    {
+        public static void Configure()
+        {
+            CommandSystem.Register("Pets", AccessLevel.Player, Pets_OnCommand);
+        }
+        [Usage("Pets"), Description("Displays your currently-active pets.")]
+        public static void Pets_OnCommand(CommandEventArgs e)
+        {
+            var pm = e.Mobile as PlayerMobile;
+            if (pm == null) return;
+            var pmAllFollowers = pm.AllFollowers;
+            if (pmAllFollowers is not null)
+                foreach (var pet in pmAllFollowers)
+                {
+                    if (pet is BaseCreature bc)
+                    {
+                        try
+                        {
+                            if (bc == pm.Mount)
+                            {
+                                e.Mobile.SendMessage($"Mount: {bc.Name} - {bc.ControlSlots} slots {pm.X}x{pm.Y}x{pm.Z} {pm.Map.Name}");
+                            }
+                            else
+                            {
+                                e.Mobile.SendMessage($"Pet: {bc.Name} - {bc.ControlSlots} slots {bc.X}x{bc.Y}x{bc.Z} {bc.Map.Name}");
+                            }
+                        }
+                        catch { }
+                    }
+                }
+            //e.Mobile.SendGump(new PetsGump(e.Mobile));
+        }
+    }
+
+    //internal class PetsGump : BaseGump
+    //{
+    //    private Mobile mobile;
+
+    //    public PetsGump(Mobile mobile)
+    //    {
+    //        this.mobile = mobile;
+    //    }
+    //}
+}
